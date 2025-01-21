715d95b2c78f284a04cb0b04dce4af84551d9462
Sebastian Wallis <j.sebastian.aw@gmail.com>
2025-01-21 13:19:21 -0500

    Refactor database connection to use environment variables and add dotenv support

--- api/libs/postgres.pool.js
+++ api/libs/postgres.pool.js
@@ -2,23 +2,13 @@
 // This file is used to create a connection to the database
 
 import pkg from 'pg';
+import { config } from '../config/config.js';
 
 const { Pool } = pkg;
 
-/**
- * Establishes and returns a connection to the PostgreSQL database
- * @async
- * @function getConnection
- * @returns {Promise<Client>} A promise that resolves to a connected PostgreSQL client instance
- * @throws {Error} When the connection cannot be established
- */
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-
-export const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'wally',
-  password: 'wally',
-  database: 'my_store'
-})
+export const pool = new Pool({connectionString: URI});
 
